c99e0773fd15f8201c3b7ba019344d5b4660e5a5
AteetGogineni <ateet.gogineni@yesgnome.com>
2022-04-25 11:12:24 +0530

    done with player controller and mouse look.......

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController controller;
+    public float playerSpeed = 12f;
+    public Transform gunFirePoint;
+    Animator animator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        animator.SetTrigger("isIdle");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetTrigger("isIdle");
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            animator.SetTrigger("isRunning");
+            PlayerRun(xInput, zInput);
+        }
+    }
+
+    private void PlayerRun(float xInput, float zInput)
+    {
+        Vector3 move = transform.right * xInput + transform.forward * zInput;
+        controller.Move(move * playerSpeed * Time.deltaTime);
+
+    }
+
+}
